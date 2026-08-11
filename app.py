7bf16be6f7ec555964bb2c36746f640e70845fa3
abdul26q <abdul26q@gmail.com>
2026-08-11 14:01:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 st.set_page_config(
     page_title="Protect the Trace. Power the Design.", 
     layout="wide",
-    page_icon="🛡️",
+    page_icon="X",
     initial_sidebar_state="expanded"
 )
 
